33273abcd4e44efa2e231eced7ae32e293a97d77
peterwu99 <peter.yh.wu@gmail.com>
2016-07-08 13:50:45 -0700

    Rejected commit: Default

--- app/src/main/java/com/example/peterwu/pyrames_android_application/MainActivity.java
+++ app/src/main/java/com/example/peterwu/pyrames_android_application/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.peterwu.pyrames_android_application;
 
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+//import com.jjoe64.graphview_demos.MainActivity;
+//import com.jjoe64.graphview_demos.R;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    BluetoothAdapter bluetooth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        System.out.println("Entered onCreate");
+
+/*
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test push
+
+        bluetooth  = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetooth != null)
+        {
+            System.out.println("Hello");
+            // Continue with bluetooth setup.
+        }
     }
+
+
 }
