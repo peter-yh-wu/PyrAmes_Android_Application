92507d9beebd1b0865880c28e179f34d365fd073
peterwu99 <peter.yh.wu@gmail.com>
2016-08-28 16:25:36 -0700

    asdf

--- app/src/main/java/com/example/peterwu/pyrames_android_application/MainActivity.java
+++ app/src/main/java/com/example/peterwu/pyrames_android_application/MainActivity.java
@@ -29,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     private  LineGraphSeries<DataPoint> series1;
 
+    //Viewport variables
+    int maxY2 = 50;
+    int minY2 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,18 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Entered onCreate");
 
-        //get graphview instance
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        //data
-        series = new LineGraphSeries<DataPoint>();
-        graph.addSeries(series);
-        //customize viewport
-        Viewport viewport = graph.getViewport();
-        viewport.setYAxisBoundsManual(true);
-        viewport.setMinY(0);
-        viewport.setMaxY(10);
-        viewport.setScrollable(true);
-
+        //Graphs
         //graphview 1
         GraphView graph1 = (GraphView) findViewById(R.id.graph1);
         //data
@@ -58,15 +51,49 @@ public class MainActivity extends AppCompatActivity {
         viewport1.setYAxisBoundsManual(true);
         viewport1.setMinY(-5);
         viewport1.setMaxY(5);
-        viewport.setScrollable(true);
+        viewport1.setScrollable(true);
 
-        //Button
+        //get graphview instance
+        GraphView graph2 = (GraphView) findViewById(R.id.graph2);
+        //data
+        series = new LineGraphSeries<DataPoint>();
+        graph2.addSeries(series);
+        //customize viewport
+        final Viewport viewport2 = graph2.getViewport();
+        viewport2.setYAxisBoundsManual(true);
+        viewport2.setMinY(minY2);
+        viewport2.setMaxY(maxY2);
+        viewport2.setScrollable(true);
+        viewport2.setScalable(true);
+
+        //Buttons
         //
-        final Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        //
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 System.out.println("Button Pressed");
+            }
+        });
+
+
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                maxY2 += 1;
+                minY2 += 1;
+                viewport2.setMaxY(maxY2);
+                viewport2.setMinY(minY2);
+            }
+        });
+        final Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                maxY2 -= 1;
+                minY2 -= 1;
+                viewport2.setMaxY(maxY2);
+                viewport2.setMinY(minY2);
             }
         });
 
